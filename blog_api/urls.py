0126a66bb3b54e91048db40d45011eb7bf4c1122
NigaraOmarova <nigara909095@gmail.com>
2021-06-03 21:22:30 +0600

    initial project

---
+++ blog_api/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # TODO add path to users endpoint
+    path('users/', views.UserListView.as_view()),
+    path ('users/<int:pk>/', views.UserDetailView.as_view()),
+    path('posts/create/', views.PostCreateView.as_view()),
+    path('posts/', views.PostListView.as_view()),
+    path('posts/<int:pk>/', views.PostDetailView.as_view()),
+    path('posts/update/<int:pk>/', views.PostUpdateView.as_view()),
+    path('posts/delete/<int:pk>/', views.PostDeleteView.as_view()),
+    path('comments/', views.CommentListCreateView.as_view()),
+    path('comments/<int:pk>/', views.CommentDetailView.as_view()),
+    path('categories/', views.CategoryView.as_view()),
+]
